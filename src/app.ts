123d451fd08ea0de1b3908d406c06288fa4c7b6e
area123 <kjs0713kjs@naver.com>
2020-10-14 03:13:23 +0900

    front-end 연결

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import { createConnection } from 'typeorm';
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -25,5 +28,14 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../public');
+app.use(serve(buildDirectory));
+
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 export default app;
