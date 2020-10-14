b9fb276ba768761d4889319445dc0e8932edfe54
area123 <kjs0713kjs@naver.com>
2020-10-14 20:32:06 +0900

    front-end 추가

---
+++ public/precache-manifest.73e2e07b8dc1be6711997c49d7ebe802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1abc277d0882d94da9",
+    "url": "/css/app.72f96817.css"
+  },
+  {
+    "revision": "677d2e53ae4d85ee2c41d3cae3d2ed59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a1abc277d0882d94da9",
+    "url": "/js/app.899fee73.js"
+  },
+  {
+    "revision": "a2fc3d40a8aff50c952f",
+    "url": "/js/chunk-vendors.0e6de847.js"
+  },
+  {
+    "revision": "b55dfa5992931726ae1c47555c258aa1",
+    "url": "/manifest.json"
+  }
+]);
